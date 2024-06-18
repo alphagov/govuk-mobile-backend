04e96859a308a18fa16f8f739565d14f86cf2703
James Miller <james.miller1@digital.cabinet-office.gov.uk>
2024-06-18 15:51:26 +0100

    GOVUKAPP-6 remove autogenerated CDK code

--- applications/lib/govuk-mobile-backend-stack.ts
+++ applications/lib/govuk-mobile-backend-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { HelloWorld } from './services/hello-world';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GovukMobileBackendStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
