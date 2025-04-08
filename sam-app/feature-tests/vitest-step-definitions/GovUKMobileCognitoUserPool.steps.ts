f630315ef937acf75a92b8bb56c4e7e0a3db3d58
Brian Bayes <17807978+brianbayes@users.noreply.github.com>
2025-04-08 13:41:22 +0100

    fix: formatting

--- sam-app/feature-tests/vitest-step-definitions/GovUKMobileCognitoUserPool.steps.ts
+++ sam-app/feature-tests/vitest-step-definitions/GovUKMobileCognitoUserPool.steps.ts
@@ -1,74 +1,87 @@
-import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber"
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import { schema } from 'yaml-cfn';
-import { readFileSync } from 'fs';
-import { load } from 'js-yaml';
+import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import { schema } from "yaml-cfn";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
 
-const feature = await loadFeature('feature-tests/vitest-features/GovUKMobileCognitoUserPool.feature')
+const feature = await loadFeature(
+  "feature-tests/vitest-features/GovUKMobileCognitoUserPool.feature"
+);
 
 let template: Template;
 
 describeFeature(feature, ({ BeforeAllScenarios, Scenario }) => {
-    BeforeAllScenarios(() => {
-        let yamltemplate = load(readFileSync('template.yaml', 'utf-8'), { schema: schema });
-        template = Template.fromJSON(yamltemplate);
-    })
-    Scenario(`A template can deploy the GOV UK Mobile Cognito Userpool`, ({ Given, Then }) => {
-        Given(`a template to deploy the GOV UK Mobile Cognito Userpool`, () => {
-        })
-        Then(`the template must have the required resource and properties to deploy the GOV UK Mobile Cognito Userpool`, () => {
-            template.hasResourceProperties(
-                "AWS::Cognito::UserPool",
-                Match.objectEquals({
-                    UserPoolName: {
-                        "Fn::Join": [
+  BeforeAllScenarios(() => {
+    let yamltemplate = load(readFileSync("template.yaml", "utf-8"), {
+      schema: schema,
+    });
+    template = Template.fromJSON(yamltemplate);
+  });
+  Scenario(
+    `A template can deploy the GOV UK Mobile Cognito Userpool`,
+    ({ Given, Then }) => {
+      Given(
+        `a template to deploy the GOV UK Mobile Cognito Userpool`,
+        () => {}
+      );
+      Then(
+        `the template must have the required resource and properties to deploy the GOV UK Mobile Cognito Userpool`,
+        () => {
+          template.hasResourceProperties(
+            "AWS::Cognito::UserPool",
+            Match.objectEquals({
+              UserPoolName: {
+                "Fn::Join": [
+                  "-",
+                  [
+                    {
+                      Ref: "AWS::StackName",
+                    },
+                    "user-pool",
+                    {
+                      "Fn::Select": [
+                        4,
+                        {
+                          "Fn::Split": [
                             "-",
-                            [
+                            {
+                              "Fn::Select": [
+                                2,
                                 {
-                                    Ref: "AWS::StackName"
+                                  "Fn::Split": [
+                                    "/",
+                                    {
+                                      Ref: "AWS::StackId",
+                                    },
+                                  ],
                                 },
-                                "user-pool",
-                                {
-                                    "Fn::Select": [
-                                        4,
-                                        {
-                                            "Fn::Split": [
-                                                "-",
-                                                {
-                                                    "Fn::Select": [
-                                                        2,
-                                                        {
-                                                            "Fn::Split": [
-                                                                "/",
-                                                                {
-                                                                    Ref: "AWS::StackId"
-                                                                }
-                                                            ]
-                                                        }
-                                                    ]
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                }
-                            ]
-                        ],
+                              ],
+                            },
+                          ],
+                        },
+                      ],
                     },
-                    DeletionProtection: "ACTIVE",
-                    UsernameAttributes: ["email"],
-                    Schema: [{
-                        "AttributeDataType": "String",
-                        "Name": "email",
-                        "Required": true,
-                    }],
-                    UserPoolTags: {
-                        "Environment": { "Ref": "Environment" },
-                        "Product": "GOV.UK",
-                        "System": "Authentication"
-                    },
-                }))
-
-        })
-    })
-})
-
+                  ],
+                ],
+              },
+              DeletionProtection: "ACTIVE",
+              UsernameAttributes: ["email"],
+              Schema: [
+                {
+                  AttributeDataType: "String",
+                  Name: "email",
+                  Required: true,
+                },
+              ],
+              UserPoolTags: {
+                Environment: { Ref: "Environment" },
+                Product: "GOV.UK",
+                System: "Authentication",
+              },
+            })
+          );
+        }
+      );
+    }
+  );
+});
--- sam-app/feature-tests/vitest-step-definitions/GovUKMobileWebApplicationFirewall.steps.ts
+++ sam-app/feature-tests/vitest-step-definitions/GovUKMobileWebApplicationFirewall.steps.ts
@@ -1,75 +1,87 @@
-import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber"
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import { schema } from 'yaml-cfn';
-import { readFileSync } from 'fs';
-import { load } from 'js-yaml';
+import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import { schema } from "yaml-cfn";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
 
-const feature = await loadFeature('feature-tests/vitest-features/GovUKMobileWebApplicationFirewall.feature');
+const feature = await loadFeature(
+  "feature-tests/vitest-features/GovUKMobileWebApplicationFirewall.feature"
+);
 
 let template: Template;
 
 describeFeature(feature, ({ BeforeAllScenarios, Scenario }) => {
-    BeforeAllScenarios(() => {
-        let yamltemplate = load(readFileSync('template.yaml', 'utf-8'), { schema: schema });
-        template = Template.fromJSON(yamltemplate);
+  BeforeAllScenarios(() => {
+    let yamltemplate = load(readFileSync("template.yaml", "utf-8"), {
+      schema: schema,
     });
+    template = Template.fromJSON(yamltemplate);
+  });
 
-    Scenario(`A template can deploy the GOV UK Mobile Web Application Firewall`, ({ Given, Then }) => {
-        Given(`a template to deploy the GOV UK Mobile Web Application Firewall`, () => { })
-        Then(`the template must have the required resource and properties to deploy the GOV UK Mobile Web Application Firewall`, () => {
-
-            template.hasResourceProperties(
-                "AWS::WAFv2::WebACL",
-                Match.objectEquals({
-                    DefaultAction: {
-                        Allow: {}
-                    },
-                    Scope: "REGIONAL",
-                    VisibilityConfig: {
-                        CloudWatchMetricsEnabled: true,
-                        MetricName: {
-                            "Fn::Join": [
-                                "-",
-                                [
-                                    {
-                                        Ref: "AWS::StackName"
-                                    },
-                                    "waf-acl-rules",
-                                    {
-                                        "Fn::Select": [
-                                            4,
-                                            {
-                                                "Fn::Split": [
-                                                    "-",
-                                                    {
-                                                        "Fn::Select": [
-                                                            2,
-                                                            {
-                                                                "Fn::Split": [
-                                                                    "/",
-                                                                    {
-                                                                        Ref: "AWS::StackId"
-                                                                    }
-                                                                ]
-                                                            }
-                                                        ]
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
+  Scenario(
+    `A template can deploy the GOV UK Mobile Web Application Firewall`,
+    ({ Given, Then }) => {
+      Given(
+        `a template to deploy the GOV UK Mobile Web Application Firewall`,
+        () => {}
+      );
+      Then(
+        `the template must have the required resource and properties to deploy the GOV UK Mobile Web Application Firewall`,
+        () => {
+          template.hasResourceProperties(
+            "AWS::WAFv2::WebACL",
+            Match.objectEquals({
+              DefaultAction: {
+                Allow: {},
+              },
+              Scope: "REGIONAL",
+              VisibilityConfig: {
+                CloudWatchMetricsEnabled: true,
+                MetricName: {
+                  "Fn::Join": [
+                    "-",
+                    [
+                      {
+                        Ref: "AWS::StackName",
+                      },
+                      "waf-acl-rules",
+                      {
+                        "Fn::Select": [
+                          4,
+                          {
+                            "Fn::Split": [
+                              "-",
+                              {
+                                "Fn::Select": [
+                                  2,
+                                  {
+                                    "Fn::Split": [
+                                      "/",
+                                      {
+                                        Ref: "AWS::StackId",
+                                      },
+                                    ],
+                                  },
+                                ],
+                              },
                             ],
-                        },
-                        SampledRequestsEnabled: true
-                    },
-                    Tags: [
-                        { Key: "Product", Value: "GOV.UK" },
-                        { Key: "Environment", Value: { Ref: "Environment" } },
-                        { Key: "System", Value: "Authentication" },
-                    ]
-                })
-            );
-        })
-    });
+                          },
+                        ],
+                      },
+                    ],
+                  ],
+                },
+                SampledRequestsEnabled: true,
+              },
+              Tags: [
+                { Key: "Product", Value: "GOV.UK" },
+                { Key: "Environment", Value: { Ref: "Environment" } },
+                { Key: "System", Value: "Authentication" },
+              ],
+            })
+          );
+        }
+      );
+    }
+  );
 });
