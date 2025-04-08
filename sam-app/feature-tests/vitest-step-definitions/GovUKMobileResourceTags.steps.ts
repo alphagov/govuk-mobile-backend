63c39a98bc29efe7b681011f6d65a85c3345a654
Brian Bayes <17807978+brianbayes@users.noreply.github.com>
2025-04-08 13:38:00 +0100

    chore: add basic api gateway to template.yaml

--- sam-app/feature-tests/vitest-step-definitions/GovUKMobileResourceTags.steps.ts
+++ sam-app/feature-tests/vitest-step-definitions/GovUKMobileResourceTags.steps.ts
@@ -1,48 +1,49 @@
-import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber"
-import { Template } from 'aws-cdk-lib/assertions';
-import { schema } from 'yaml-cfn';
-import { readFileSync } from 'fs';
-import { load } from 'js-yaml';
-import { expect } from 'vitest';
+import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber";
+import { Template } from "aws-cdk-lib/assertions";
+import { schema } from "yaml-cfn";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
+import { expect } from "vitest";
 
-const feature = await loadFeature('feature-tests/vitest-features/GovUKMobileResourceTags.feature')
+const feature = await loadFeature(
+  "feature-tests/vitest-features/GovUKMobileResourceTags.feature"
+);
 
 let template: Template;
 
 const ignoredResources = [
-    "GovUKMobileCognitoUserPool",
-    "GovUKMobileCognitoWAFAssociation",
-    "GovUKMobileApiGateway"
+  "GovUKMobileCognitoUserPool",
+  "GovUKMobileCognitoWAFAssociation",
+  "GovUKMobileApiGateway",
 ];
 
 describeFeature(feature, ({ BeforeAllScenarios, Scenario }) => {
-    BeforeAllScenarios(() => {
-        let yamltemplate = load(readFileSync('template.yaml', 'utf-8'), { schema: schema });
-        template = Template.fromJSON(yamltemplate);
+  BeforeAllScenarios(() => {
+    let yamltemplate = load(readFileSync("template.yaml", "utf-8"), {
+      schema: schema,
     });
+    template = Template.fromJSON(yamltemplate);
+  });
 
-    Scenario(`A template has the correct resource tags`, ({ Given, Then }) => {
-        Given(`a template with AWS resources`, () => { })
-        Then(`the template's resources must have the required tags`, () => {
-            Object.keys(template.toJSON().Resources).forEach((resourceName) => {
+  Scenario(`A template has the correct resource tags`, ({ Given, Then }) => {
+    Given(`a template with AWS resources`, () => {});
+    Then(`the template's resources must have the required tags`, () => {
+      Object.keys(template.toJSON().Resources).forEach((resourceName) => {
+        const resource = template.toJSON().Resources[resourceName];
 
-                const resource = template.toJSON().Resources[resourceName];
+        if (!ignoredResources.includes(resourceName)) {
+          expect(resource.Properties).toHaveProperty("Tags");
 
-                if (!ignoredResources.includes(resourceName)) {
+          const actualTags = resource.Properties["Tags"];
+          const expectedTags = [
+            { Key: "Product", Value: "GOV.UK" },
+            { Key: "Environment", Value: expect.any(Object) },
+            { Key: "System", Value: "Authentication" },
+          ];
 
-                    expect(resource.Properties).toHaveProperty('Tags');
-
-                    const actualTags = resource.Properties["Tags"];
-                    const expectedTags = [
-                        { Key: 'Product', Value: 'GOV.UK' },
-                        { Key: 'Environment', Value: expect.any(Object) },
-                        { Key: 'System', Value: 'Authentication' }
-                    ];
-
-                    expect(actualTags).toEqual(expectedTags);
-                }
-            })
-        })
+          expect(actualTags).toEqual(expectedTags);
+        }
+      });
     });
-
-})
+  });
+});
