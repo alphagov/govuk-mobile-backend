85edbca8d100bddb2eb63e5ebbbee3de1e34b2c6
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-04-10 16:24:46 +0100

    fix: ignore no empty funcs for feature file tests

--- sam-app/feature-tests/vitest-step-definitions/GovUKMobileApiGateway.steps.ts
+++ sam-app/feature-tests/vitest-step-definitions/GovUKMobileApiGateway.steps.ts
@@ -20,7 +20,7 @@ describeFeature(feature, ({ BeforeAllScenarios, Scenario }) => {
   Scenario(
     `A template can deploy the GOV UK Api Gateway`,
     ({ Given, Then }) => {
-      Given(`a template to deploy the GOV UK Api Gateway`);
+      Given(`a template to deploy the GOV UK Api Gateway`, () => {});
       Then(
         `the template must have the required resource and properties to deploy the GOV UK Api Gateway`,
         () => {
