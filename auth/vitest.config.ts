b781ee50a0461601633be5a4a3fc9a3af852d0ab
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-04-11 09:08:19 +0100

    fix: debug test infra run

--- auth/vitest.config.ts
+++ auth/vitest.config.ts
@@ -5,6 +5,15 @@ export default defineConfig({
     test: {
         include: [
             "**/*.steps.ts",
-        ]
+        ],
+        reporters: [
+            'default', // To still see output in the console
+            ['json', { outputFile: './report/results.json' }],
+            ['junit', { outputFile: './report/junit.xml' }],
+        ],
+        coverage: {
+            provider: 'istanbul', // or 'v8'
+            reporter: ['text', 'json', 'html'],
+        },
     }
 })
