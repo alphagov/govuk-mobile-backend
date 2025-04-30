208101eaf66fa4612cf251805c096fda62f5029d
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-04-30 11:41:15 +0100

    refactor  add unit tests for aws::cognito::userpoolidentityprovider
    scope: unit tests
    description: Unit tests for AWS::Cognito::UserPoolIdentityProvider to enusre all
    properties are set properly
    
    footer:
    JIRA-ID:GOVUKAPP-1513:

---
+++ auth/tests/unit/cognito-ipd.unit.test.ts
@@ -0,0 +1,125 @@
+import { Template, Capture, Match, MatchResult } from "aws-cdk-lib/assertions";
+import { schema } from "yaml-cfn";
+import { describe, it, beforeAll } from "vitest";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
+
+let template: Template;
+
+describe("Set up the Cognito User Pool Identity Provider for GovUK app", () => {
+  beforeAll(() => {
+    let yamltemplate: string = load(readFileSync("template.yaml", "utf-8"), {
+      schema: schema,
+    });
+    template = Template.fromJSON(yamltemplate);
+  });
+  it("has an associated user pool", () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      UserPoolId: {
+        Ref: "CognitoUserPool",
+      },
+    });
+  });
+  it("has a provider name", () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      ProviderName: "onelogin",
+    });
+  });
+  it("has a provider type OIDC", () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      ProviderType: "OIDC",
+    });
+  });
+  it("has provider details", () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      ProviderDetails: {
+        attributes_request_method: "GET",
+        attributes_url: {
+          "Fn::Join": [
+            "",
+            [
+              "https://oidc.",
+              {
+                "Fn::FindInMap": [
+                  "OneLogin",
+                  "Environment",
+                  { Ref: "Environment" },
+                ],
+              },
+              ".account.gov.uk/userinfo",
+            ],
+          ],
+        },
+        attributes_url_add_attributes: false,
+        authorize_scopes: "openid email",
+        authorize_url: {
+          "Fn::Join": [
+            "",
+            [
+              "https://oidc.",
+              {
+                "Fn::FindInMap": [
+                  "OneLogin",
+                  "Environment",
+                  { Ref: "Environment" },
+                ],
+              },
+              ".account.gov.uk/authorize",
+            ],
+          ],
+        },
+        client_id: "{{resolve:ssm:/onelogin/client_id}}",
+        client_secret:
+          "{{resolve:secretsmanager:/onelogin/client_secret:SecretString}}",
+        jwks_uri: {
+          "Fn::Join": [
+            "",
+            [
+              "https://oidc.",
+              {
+                "Fn::FindInMap": [
+                  "OneLogin",
+                  "Environment",
+                  { Ref: "Environment" },
+                ],
+              },
+              ".account.gov.uk/.well-known/jwks.json",
+            ],
+          ],
+        },
+        oidc_issuer: {
+          "Fn::Join": [
+            "",
+            [
+              "https://oidc.",
+              {
+                "Fn::FindInMap": [
+                  "OneLogin",
+                  "Environment",
+                  { Ref: "Environment" },
+                ],
+              },
+              ".account.gov.uk",
+            ],
+          ],
+        },
+        token_url: {
+          "Fn::Join": [
+            "",
+            [
+              "https://oidc.",
+              {
+                "Fn::FindInMap": [
+                  "OneLogin",
+                  "Environment",
+                  { Ref: "Environment" },
+                ],
+              },
+              ".account.gov.uk/token",
+            ],
+          ],
+        },
+      },
+    });
+  });
+});
