eca2a6e631a8e4a908c74bff2685c6a6df0f5e7f
Charles Stevenson <charles.stevenson@digital.cabinet-office.gov.uk>
2025-05-01 12:08:24 +0100

    config :boom: adding sonarqube static code analysis to repo
    scope: ci pipeline
    description: Added step to ci pipeline to run sonarqube analysis step
    
    footer:
    JIRA-ID:GOVUKAPP-1611:
    
    Added missing sonar properties file
    
    sonar exclusion properties
    
    added test coverage
    
    added test coverage
    
    added test coverage
    
    skip feature tests to get coverage to run
    
    Changed running order of ci.yaml
    
    tweaks to nx bootstrapping
    
    fixed missing commas in project.json
    
    linting config fixes
    
    added sam:validate and :lint to ci
    
    fixed pre-commit linting and unit testing step
    
    added sam:validate and sam:lint to pre-commit
    
    Linting error workarounds
    
    sam validate region setting in pipeline
    
    sam validate region setting in pipeline
    
    Fix sonar not picking up coverage report
    
    fix for sonar analysing feature-tests
    
    fix for sonar analysing feature-tests
    
    fix for vitest coverage exclusions

--- auth/vitest.config.ts
+++ auth/vitest.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from "vitest/config"
-import { VitestCucumberPlugin } from "@amiceli/vitest-cucumber"
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         include: [
-            "**/*.steps.ts",
-            "**/*.test.ts",
+          "**/*.steps.ts",
+          "**/*.test.ts",
         ],
         reporters: [
-            'default', // To still see output in the console
-            ['json', { outputFile: './report/results.json' }],
-            ['junit', { outputFile: './report/junit.xml' }],
+          'default', // To still see output in the console
+          ['json', { outputFile: './report/results.json' }],
+          ['junit', { outputFile: './report/junit.xml' }]
         ],
         coverage: {
-            provider: 'istanbul', // or 'v8'
-            reporter: ['text', 'json', 'html'],
+          provider: 'istanbul', // or 'v8'
+          reporter: ['text', 'json', 'html', 'lcov'],
+          reportOnFailure: true,
+          exclude: ["**/feature-tests/**", "**/test?/**"]
         },
         watch: false
     }
