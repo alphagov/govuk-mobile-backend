ba7c4f4e92de956015f26261fc4d4cb73fea86fe
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-05-16 08:39:21 +0100

    refactor  vitest refactoring for mono-repo
    scope: vitest
    description: Refactoring multiple vitest configs into root or package-root
    
    footer: 1611
    JIRA-ID:GOVUKAPP-1611:
    
    chore: merge auth vitest and root vitest configs
    
    fix: remove unusued vitest cucumber plugin since its optional
    
    fix: ignore nx boundaries temporarily
    
    fix: move feature tests to root config
    
    fix: move feature tests to root config

--- auth/vitest.config.ts
+++ auth/vitest.config.ts
@@ -1,32 +1,8 @@
-import { defineConfig, coverageConfigDefaults } from "vitest/config"
-import { VitestCucumberPlugin } from "@amiceli/vitest-cucumber"
+import { defineProject, mergeConfig } from "vitest/config"
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import configShared from '../vitest.config'
 
-export default defineConfig({
-    test: {
-        include: [
-            "**/*.steps.ts",
-            "**/*.test.ts",
-        ],
-        coverage: {
-          exclude: [
-            ...coverageConfigDefaults.exclude,
-            "**/feature-tests/**",
-            "**/feature-tests/**",
-            "**/tests/**/*",
-            "**/*.test.ts",
-            "vitest*.config.ts",
-            "__mocks__",
-          ],
-          reporter: ["text", "lcov"],
-          reportOnFailure: true,
-          reportsDirectory: './coverage',
-          thresholds: {
-            functions: 80,
-            lines: 80,
-            branches: 80,
-            statements: 80,
-          },
-        },
-        watch: false
-    }
-})
+export default mergeConfig(
+  configShared,
+  defineProject({})
+)
