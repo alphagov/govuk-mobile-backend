cc0c9204f1750baca0042dd1548b6616a67ee31a
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-05-19 16:01:39 +0100

    chore: add unit test to pager duty test handler

--- auth/pager-duty-test-endpoint/app.ts
+++ auth/pager-duty-test-endpoint/app.ts
@@ -3,3 +3,5 @@ import { SNSEvent } from "aws-lambda";
 export const lambdaHandler = async (event: SNSEvent): Promise<void> => {
   console.log("SNS event for test Pager Duty:", JSON.stringify(event, null, 2));
 };
+
+
