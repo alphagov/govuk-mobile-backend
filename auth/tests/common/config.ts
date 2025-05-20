51e9ea79eb02470eb4b2155ac67f7dc0500f43cd
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-05-20 14:14:58 +0100

    feat  int and acc tests for cloudwatch alarms
    scope: Cloudwatch Alarms
    description: int and acc tests for CloudWatch Alarms
    
    footer:
    JIRA-ID:GOVUKAPP-1560:

--- auth/tests/common/config.ts
+++ auth/tests/common/config.ts
@@ -1,15 +1,25 @@
-import "dotenv/config"
+import "dotenv/config";
 
 export const testConfig = {
-    authProxyFunctionName: process.env.CFN_AuthProxyFunctionName!,
-    authProxyLogGroup: process.env.CFN_AuthProxyLogGroupName!,
-    userPoolId: process.env.CFN_UserPoolId!,
-    clientId: process.env.CFN_AppUserPoolClientId!,
-    testEnvironment: process.env.TEST_ENVIRONMENT!,
-    cognitoWafLogGroupName: process.env.CFN_CognitoWafLogGroupName!,
-    sharedSignalsEndpoint: process.env.CFN_SharedSignalsEndpoint!,
-    sharedSignalsReceiverLogGroupName: process.env.CFN_SharedSignalsReceiverLogGroupName!,
-    sharedSignalsApiId: process.env.CFN_SharedSignalsApiId!,
-    postAuthenticationLambda: process.env.CFN_PostAuthenticationLambdaName!,
-    PostAuthenticationFunctionInvokePermission: process.env.CFN_PostAuthenticationFunctionInvokePermission!,
-}
+  authProxyFunctionName: process.env.CFN_AuthProxyFunctionName!,
+  authProxyLogGroup: process.env.CFN_AuthProxyLogGroupName!,
+  userPoolId: process.env.CFN_UserPoolId!,
+  clientId: process.env.CFN_AppUserPoolClientId!,
+  testEnvironment: process.env.TEST_ENVIRONMENT!,
+  cognitoWafLogGroupName: process.env.CFN_CognitoWafLogGroupName!,
+  sharedSignalsEndpoint: process.env.CFN_SharedSignalsEndpoint!,
+  sharedSignalsReceiverLogGroupName:
+    process.env.CFN_SharedSignalsReceiverLogGroupName!,
+  sharedSignalsApiId: process.env.CFN_SharedSignalsApiId!,
+  postAuthenticationLambda: process.env.CFN_PostAuthenticationLambdaName!,
+  PostAuthenticationFunctionInvokePermission:
+    process.env.CFN_PostAuthenticationFunctionInvokePermission!,
+  CloudWatchAlarmSignUpThrottlesName:
+    process.env.CFN_CloudWatchAlarmSignUpThrottlesName!,
+  CloudWatchAlarmSignInThrottlesName:
+    process.env.CFN_CloudWatchAlarmSignInThrottlesName!,
+  CloudWatchAlarmTokenRefreshThrottlesName:
+    process.env.CFN_CloudWatchAlarmTokenRefreshThrottlesName!,
+  CloudWatchAlarmFederationThrottlesName:
+    process.env.CFN_CloudWatchAlarmFederationThrottlesName!,
+};
