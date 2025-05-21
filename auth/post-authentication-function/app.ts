b04da610a1491ac3981e4a64b5afa2046e5b3df5
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-05-21 13:45:25 +0100

    chore: resolve no await expression on function

--- auth/post-authentication-function/app.ts
+++ auth/post-authentication-function/app.ts
@@ -3,14 +3,13 @@ import type { PostAuthenticationTriggerEvent } from 'aws-lambda';
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {object} event - API Gateway Lambda Proxy Input Format
  *
  * Return doc: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-post-authentication.html
- * @returns {Object} object - Post Authentication Trigger Event
- *
+ * @returns {object} object - Post Authentication Trigger Event
  */
 
-export const lambdaHandler = async (event: PostAuthenticationTriggerEvent): Promise<PostAuthenticationTriggerEvent> => {
+export const lambdaHandler = (event: PostAuthenticationTriggerEvent): PostAuthenticationTriggerEvent => {
     console.log(`Source = ${event.triggerSource}`);
     console.log(`User Pool Id = ${event.userPoolId}`);
     console.log(`Client Id = ${event.callerContext.clientId}`);
