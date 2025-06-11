885f614bdc6ffbe7e79647fa57653f02914edda9
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-06-11 08:22:14 +0100

    fix: sort rebase issues

--- auth/tests/unit/attestation.unit.test.ts
+++ auth/tests/unit/attestation.unit.test.ts
@@ -8,202 +8,225 @@ const template = loadTemplateFromFile(
 
 describe("attestation", () => {
   describe("auth proxy function", () => {
+    const lambda = template.findResources("AWS::Serverless::Function")[
+      "AuthProxyFunction"
+    ] as any;
     it("should only have an oauth token endpoint", () => {
-      const lambda = template.findResources("AWS::Serverless::Function")[
-        "AuthProxyFunction"
-      ] as any;
       expect(lambda.Properties.Events.ApiEvent.Properties.Path).toBe(
         "/oauth2/token"
       );
       expect(lambda.Properties.Events.ApiEvent.Properties.Method).toBe("post");
     });
+    it("contains a reference to the cognito secret name", () => {
+      expect(lambda.Properties.Environment.Variables).containSubset({
+        COGNITO_SECRET_NAME: {
+          "Fn::Sub": "/${ConfigStackName}/cognito/client-secret",
+        },
+      });
+    });
+
+    it("contains a reference cognito custom domain config name", () => {
+      expect(lambda.Properties.Environment.Variables).containSubset({
+        COGNITO_CUSTOM_DOMAIN_SSM_NAME: {
+          "Fn::Sub": "/${ConfigStackName}/cognito/custom-domain",
+        },
+      });
+    });
+
+    it("contains a reference to AWS region", () => {
+      expect(lambda.Properties.Environment.Variables).containSubset({
+        REGION: {
+          Ref: "AWS::Region",
+        },
+      });
+    });
+  });
+});
+
+describe("auth proxy function iam role", () => {
+  const role = template.findResources("AWS::IAM::Role")[
+    "AuthProxyFunctionIAMRole"
+  ] as any;
+  it("should have the correct trust policy", () => {
+    expect(role.Properties.AssumeRolePolicyDocument).toEqual({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          Principal: {
+            Service: "lambda.amazonaws.com",
+          },
+          Action: "sts:AssumeRole",
+        },
+      ],
+    });
+  });
+  it("should have permissions to access the cognito client secret", () => {
+    expect(role.Properties.Policies).toContainEqual(
+      expect.objectContaining({
+        PolicyName: "AuthProxyFunctionPolicy",
+        PolicyDocument: expect.objectContaining({
+          Statement: expect.arrayContaining([
+            expect.objectContaining({
+              Effect: "Allow",
+              Action: ["secretsmanager:GetSecretValue"],
+              Resource: {
+                "Fn::Sub":
+                  "arn:aws:secretsmanager:${AWS::Region}:${AWS::AccountId}:secret:/${ConfigStackName}/cognito/client-secret-*",
+              },
+            }),
+          ]),
+        }),
+      })
+    );
   });
 
-  describe("auth proxy function iam role", () => {
-    const role = template.findResources("AWS::IAM::Role")[
-      "AuthProxyFunctionIAMRole"
-    ] as any;
-    it("should have the correct trust policy", () => {
-      expect(role.Properties.AssumeRolePolicyDocument).toEqual({
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Effect: "Allow",
-            Principal: {
-              Service: "lambda.amazonaws.com",
+  it("should have permissions to get the cognito domain from parameter store", () => {
+    expect(role.Properties.Policies).toContainEqual(
+      expect.objectContaining({
+        PolicyName: "AuthProxyFunctionPolicy",
+        PolicyDocument: expect.objectContaining({
+          Statement: expect.arrayContaining([
+            expect.objectContaining({
+              Action: ["ssm:GetParameter"],
+              Effect: "Allow",
+              Resource: {
+                "Fn::Sub":
+                  "arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/${ConfigStackName}/cognito/custom-domain",
+              },
+            }),
+          ]),
+        }),
+      })
+    );
+  });
+
+  it("should have permissions to write to its own cloud watch logs only", () => {
+    expect(role.Properties.Policies).toContainEqual(
+      expect.objectContaining({
+        PolicyName: "AuthProxyFunctionPolicy",
+        PolicyDocument: expect.objectContaining({
+          Statement: expect.arrayContaining([
+            expect.objectContaining({
+              Action: [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+              ],
+              Effect: "Allow",
+              Resource: {
+                "Fn::Sub":
+                  "arn:${AWS::Partition}:logs:${AWS::Region}:${AWS::AccountId}:log-group:/aws/lambda/${AWS::StackName}-auth-proxy:*",
+              },
+            }),
+          ]),
+        }),
+      })
+    );
+  });
+
+  it("should have the correct tags", () => {
+    expect(role.Properties.Tags).toEqual([
+      {
+        Key: "Product",
+        Value: "GOV.UK",
+      },
+      {
+        Key: "Environment",
+        Value: {
+          Ref: "Environment",
+        },
+      },
+      {
+        Key: "System",
+        Value: "Authentication",
+      },
+    ]);
+  });
+});
+
+describe("api gateway", () => {
+  it("should provision an auth proxy api gateway", () => {
+    template.hasResourceProperties("AWS::Serverless::Api", {
+      Name: {
+        "Fn::Join": [
+          "-",
+          [
+            {
+              Ref: "AWS::StackName",
             },
-            Action: "sts:AssumeRole",
-          },
-        ],
-      });
-    });
-    it("should have permissions to access the cognito client secret", () => {
-      expect(role.Properties.Policies).toContainEqual(
-        expect.objectContaining({
-          PolicyName: "AuthProxyFunctionPolicy",
-          PolicyDocument: expect.objectContaining({
-            Statement: expect.arrayContaining([
-              expect.objectContaining({
-                Effect: "Allow",
-                Action: ["secretsmanager:GetSecretValue"],
-                Resource: {
-                  "Fn::Sub":
-                    "arn:aws:secretsmanager:${AWS::Region}:${AWS::AccountId}:secret:/${ConfigStackName}/cognito/client-secret-*",
+            "auth-proxy",
+            {
+              "Fn::Select": [
+                4,
+                {
+                  "Fn::Split": [
+                    "-",
+                    {
+                      "Fn::Select": [
+                        2,
+                        {
+                          "Fn::Split": [
+                            "/",
+                            {
+                              Ref: "AWS::StackId",
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
                 },
-              }),
-            ]),
-          }),
-        })
-      );
-    });
-
-    it("should have permissions to get the cognito domain from parameter store", () => {
-      expect(role.Properties.Policies).toContainEqual(
-        expect.objectContaining({
-          PolicyName: "AuthProxyFunctionPolicy",
-          PolicyDocument: expect.objectContaining({
-            Statement: expect.arrayContaining([
-              expect.objectContaining({
-                Action: ["ssm:GetParameter"],
-                Effect: "Allow",
-                Resource: {
-                  "Fn::Sub":
-                    "arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/${ConfigStackName}/cognito/custom-domain",
-                },
-              }),
-            ]),
-          }),
-        })
-      );
-    });
-
-    it("should have permissions to write to its own cloud watch logs only", () => {
-      expect(role.Properties.Policies).toContainEqual(
-        expect.objectContaining({
-          PolicyName: "AuthProxyFunctionPolicy",
-          PolicyDocument: expect.objectContaining({
-            Statement: expect.arrayContaining([
-              expect.objectContaining({
-                Action: [
-                  "logs:CreateLogGroup",
-                  "logs:CreateLogStream",
-                  "logs:PutLogEvents",
-                ],
-                Effect: "Allow",
-                Resource: {
-                  "Fn::Sub":
-                    "arn:${AWS::Partition}:logs:${AWS::Region}:${AWS::AccountId}:log-group:/aws/lambda/${AWS::StackName}-auth-proxy:*",
-                },
-              }),
-            ]),
-          }),
-        })
-      );
-    });
-
-    it("should have the correct tags", () => {
-      expect(role.Properties.Tags).toEqual([
-        {
-          Key: "Product",
-          Value: "GOV.UK",
-        },
-        {
-          Key: "Environment",
-          Value: {
-            Ref: "Environment",
-          },
-        },
-        {
-          Key: "System",
-          Value: "Authentication",
-        },
-      ]);
-    });
-  });
-
-  describe("api gateway", () => {
-    it("should provision an auth proxy api gateway", () => {
-      template.hasResourceProperties("AWS::Serverless::Api", {
-        Name: {
-          "Fn::Join": [
-            "-",
-            [
-              {
-                Ref: "AWS::StackName",
-              },
-              "auth-proxy",
-              {
-                "Fn::Select": [
-                  4,
-                  {
-                    "Fn::Split": [
-                      "-",
-                      {
-                        "Fn::Select": [
-                          2,
-                          {
-                            "Fn::Split": [
-                              "/",
-                              {
-                                Ref: "AWS::StackId",
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
+              ],
+            },
           ],
-        },
-      });
+        ],
+      },
+    });
+  });
+});
+
+describe("waf", () => {
+  const authProxyWaf = template.findResources("AWS::WAFv2::WebACL")[
+    "AuthProxyWaf"
+  ] as any;
+  const association = template.findResources("AWS::WAFv2::WebACLAssociation")[
+    "AuthProxyWafAssociation"
+  ] as any;
+
+  it("should allow all requests that are not match", () => {
+    expect(authProxyWaf.Properties.DefaultAction).toEqual({
+      Allow: {},
     });
   });
 
-  describe("waf", () => {
-    const authProxyWaf = template.findResources("AWS::WAFv2::WebACL")[
-      "AuthProxyWaf"
-    ] as any;
-    const association = template.findResources("AWS::WAFv2::WebACLAssociation")[
-      "AuthProxyWafAssociation"
-    ] as any;
+  it("should have a regional scope", () => {
+    expect(authProxyWaf.Properties.Scope).toEqual("REGIONAL");
+  });
 
-    it("should allow all requests that are not match", () => {
-      expect(authProxyWaf.Properties.DefaultAction).toEqual({
-        Allow: {},
-      });
-    });
-
-    it("should have a regional scope", () => {
-      expect(authProxyWaf.Properties.Scope).toEqual("REGIONAL");
-    });
-
-    it("should have the required tags", () => {
-      expect(authProxyWaf.Properties.Tags).toEqual([
-        {
-          Key: "Product",
-          Value: "GOV.UK",
+  it("should have the required tags", () => {
+    expect(authProxyWaf.Properties.Tags).toEqual([
+      {
+        Key: "Product",
+        Value: "GOV.UK",
+      },
+      {
+        Key: "Environment",
+        Value: {
+          Ref: "Environment",
         },
-        {
-          Key: "Environment",
-          Value: {
-            Ref: "Environment",
-          },
-        },
-        {
-          Key: "System",
-          Value: "Authentication",
-        },
-      ]);
-    });
+      },
+      {
+        Key: "System",
+        Value: "Authentication",
+      },
+    ]);
+  });
 
-    it("should be associated with the api gateway", () => {
-      expect(association.Properties.ResourceArn).toEqual({
-        "Fn::Sub":
-          "arn:${AWS::Partition}:apigateway:${AWS::Region}::/restapis/${AttestationProxyApi}/stages/${AttestationProxyApi.Stage}",
-      });
+  it("should be associated with the api gateway", () => {
+    expect(association.Properties.ResourceArn).toEqual({
+      "Fn::Sub":
+        "arn:${AWS::Partition}:apigateway:${AWS::Region}::/restapis/${AttestationProxyApi}/stages/${AttestationProxyApi.Stage}",
     });
   });
 });
