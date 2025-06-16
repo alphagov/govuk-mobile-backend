8b45cda7a5f22cfdbc7f85c50732b0e302cc6f12
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-06-16 11:55:44 +0100

    feat: add open api spec

--- auth/tests/api/attestation.test.ts
+++ auth/tests/api/attestation.test.ts
@@ -3,20 +3,51 @@ import axios, { AxiosError } from 'axios';
 import { testConfig } from '../common/config';
 
 describe('POST /oauth2/token', () => {
-    const baseUrl = testConfig.authProxyUrl;
-    const tokenUrl = `${baseUrl}/oauth2/token`
+	const baseUrl = testConfig.authProxyUrl;
+	const tokenUrl = `${baseUrl}/oauth2/token`
 
-	it('should satisfy OpenAPI spec', async () => {
-		await axios.post(tokenUrl, {}, {
-			headers: {
-				'Cotent-Type': 'application/json'
+	it.each([
+		[{}, {}],
+		[{
+			'Content-Type': 'application/json',
+		}, {}],
+		[
+			{
+				'Content-Type': 'application/json',
+			},
+			{
+				grant_type: 'authorization_code',
 			}
-		})
-			.catch((e: AxiosError) => {
-				expect(e.response?.status).toBe(400)
-				expect(e.response?.data).toEqual({
-					message: "Invalid request body",
-				})
+		],
+		[
+			{
+				'Content-Type': 'application/json',
+			},
+			{
+				grant_type: 'authorization_code',
+				refresh_token: 'foobar',
+				client_id: 'test-client-id',
+			}
+		],
+		[
+			{
+				'Content-Type': 'application/json',
+			},
+			{
+				grant_type: 'refresh_token',
+				refresh_token: 'foobar',
+			}
+		],
+	])
+		('should validate the request body and headers', async (headers, body) => {
+			await axios.post(tokenUrl, body, {
+				headers
 			})
-	});
+				.catch((e: AxiosError) => {
+					expect(e.response?.status).toBe(400)
+					expect(e.response?.data).toEqual({
+						message: "Invalid request body",
+					})
+				})
+		});
 });
