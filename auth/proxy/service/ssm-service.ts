a70f111fd88c966b83b6f7b93ceea28612fcec3a
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-06-20 10:51:31 +0100

    feat: cache the value of the cognito domain ssm parameter

--- auth/proxy/service/ssm-service.ts
+++ auth/proxy/service/ssm-service.ts
@@ -2,13 +2,19 @@ import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 
 export class SSMService {
     private ssmClient: SSMClient;
+    private paramterValue: null | string;
 
     public constructor(region: string) {
         this.ssmClient = new SSMClient({ region });
+        this.paramterValue = null;
     }
 
     public async getParameterValue(parameterName: string): Promise<string> {
         try {
+            if (this.paramterValue != null) {
+                return this.paramterValue
+            }
+
             const command = new GetParameterCommand({
                 Name: parameterName,
                 WithDecryption: true,
@@ -20,9 +26,11 @@ export class SSMService {
             if (value === undefined) {
                 throw new Error(`Parameter ${parameterName} not found or has no value.`);
             }
-            
+
+            this.paramterValue = value;
+
             return value;
-            
+
         } catch (error) {
             console.error(`Error fetching parameter ${parameterName}:`, error);
             throw error;
