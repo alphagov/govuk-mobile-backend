ba86b7295c742f01a2ca86687c6d797a65d58911
Surjeet Singh <surjeet.singh@digital.cabinet-office.gov.uk>
2025-06-25 16:12:01 +0100

    refactor  24 hr refresh token validity
    scope:
    description: auth refresh token validity and test cases
    
    footer:
    JIRA-ID:GOVUKAPP-2028:

--- auth/tests/unit/cognito-client-oidc.unit.test.ts
+++ auth/tests/unit/cognito-client-oidc.unit.test.ts
@@ -86,7 +86,7 @@ describe("Set up the Cognito User Pool OIDC client", () => {
     template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
       AccessTokenValidity: 300, //5 minutes
       IdTokenValidity: 3600, // one hr
-      RefreshTokenValidity: 2592000, // 30 days
+      RefreshTokenValidity: 86400, // 24 hrs
     });
   });
 
