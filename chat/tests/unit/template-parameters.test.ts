04415fed140be14be3f57e9aa3c7c7eb15777772
Brian Bayes <17807978+brianbayes@users.noreply.github.com>
2025-07-04 14:28:20 +0100

    feat: basic template.yaml for chat including unit and acc tests (#192)
    * feat: basic template.yaml for chat including unit and acc tests
    
    * chore: regenerate auth/package-lock.json

---
+++ chat/tests/unit/template-parameters.test.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from "vitest";
+import { loadTemplateFromFile } from "../common/template";
+import path from "path";
+
+const template = loadTemplateFromFile(
+  path.join(__dirname, "..", "..", "template.yaml")
+);
+
+describe("Template Parameters", () => {
+  it("should have the correct parameters", () => {
+    expect(template.findParameters("Environment")).toEqual({
+      Environment: {
+        Description: "The name of the environment to deploy to",
+        Type: "String",
+        AllowedValues: [
+          "build",
+          "staging",
+          "production",
+          "integration",
+          "local",
+          "dev",
+        ],
+      },
+    });
+    expect(template.findParameters("CodeSigningConfigArn")).toEqual({
+      CodeSigningConfigArn: {
+        Description:
+          "The ARN of the Code Signing Config to use, provided by the deployment pipeline",
+        Type: "String",
+        Default: "none",
+      },
+    });
+    expect(template.findParameters("PermissionsBoundary")).toEqual({
+      PermissionsBoundary: {
+        Description:
+          "The ARN of the permissions boundary to apply to any role created by the template",
+        Type: "String",
+        Default: "none",
+      },
+    });
+    expect(template.findParameters("ConfigStackName")).toEqual({
+      ConfigStackName: {
+        Description: "Stack Name for Config Stack",
+        Type: "String",
+        Default: "govuk-app-config",
+      },
+    });
+  });
+});
