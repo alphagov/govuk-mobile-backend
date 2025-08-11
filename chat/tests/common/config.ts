e066c6cad5787024b44629571beddf2a007a2d72
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-08-11 12:24:44 +0100

    feat: waf tests

--- chat/tests/common/config.ts
+++ chat/tests/common/config.ts
@@ -9,6 +9,7 @@ const getTestConfig = () => {
     'CFN_ChatApiGatewayId',
     'CFN_ChatApiGatewayResourceId',
     'CFN_ChatApiGatewayMethodId',
+    'CFN_ChatApiGatewayAuthorizerId',
     'CFN_ChatApiGatewayDeploymentId',
     'CFN_ChatAuthorizerFunctionName',
     'CFN_ChatAuthorizerInvokePermissionId',
@@ -25,13 +26,24 @@ const getTestConfig = () => {
   }
 
   return {
+    environment: process.env.TEST_ENVIRONMENT,
+    region: process.env.CFN_AWS_REGION || 'eu-west-2',
     awsAccountId: process.env.CFN_AWSAccountId!,
     stackName: process.env.CFN_StackName!,
+    authStackName: process.env.CFN_AuthStackName!,
+    acceptanceTestFunctionName: `${process.env.CFN_AuthStackName}-testing-function`,
     configStackName: process.env.CFN_ConfigStackName!,
     chatApiGatewayUrl: process.env.CFN_ChatApiGatewayUrl!,
     chatApiGatewayId: process.env.CFN_ChatApiGatewayId!,
     chatApiGatewayResourceId: process.env.CFN_ChatApiGatewayResourceId!,
     chatApiGatewayMethodId: process.env.CFN_ChatApiGatewayMethodId!,
+    chatApiGatewayAuthorizerId: process.env.CFN_ChatApiGatewayAuthorizerId!,
+    chatApiGatewayAccessLogGroupName:
+      process.env.CFN_ChatApiGatewayAccessLogGroupName!,
+    chatApiGatewayAccessLogGroupKMSKeyArn:
+      process.env.CFN_ChatApiGatewayAccessLogGroupKMSKeyArn!,
+    chatApiGatewayAccessLogGroupKMSKeyId:
+      process.env.CFN_ChatApiGatewayAccessLogGroupKMSKeyId!,
     chatApiGatewayDeploymentId: process.env.CFN_ChatApiGatewayDeploymentId!,
     chatAuthorizerFunctionName: process.env.CFN_ChatAuthorizerFunctionName!,
     chatAuthorizerInvokePermissionId:
