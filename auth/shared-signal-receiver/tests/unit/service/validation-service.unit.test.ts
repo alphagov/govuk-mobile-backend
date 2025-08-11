1ee782e1e9e37d0864e6079cf4d50e42e5018b63
Surjeet Singh <surjeet.singh@digital.cabinet-office.gov.uk>
2025-08-11 07:59:24 +0100

    Chore: Refactor validation logic (#252)
    * refactor validation logic
    
    * fixing ci run
    
    * refactor validation logic
    
    * refactor validation logic
    
    * fixing ci run
    
    * fixing merge conflicts
    
    * refactor validation logic
    
    * handling merge
    
    * fix: pull parameter from test instead of direct resolution of param store to avoid eager resolution in environments without the parameter
    
    * fix: is not integrated flag used an or condition - simplify by inverting the is integrated flag (#261)
    
    * feat: add health-check test
    
    * feat  adds acceptance tests for health-check function
    
    scope: shared-signals
    description: Adds acceptance tests for health-check
    
    footer: 2296
    JIRA-ID:GOVUKAPP-2296:
    
    * refactor  removes post-auth function
    
    scope: post-auth
    description: Removes post-auth function. This resource is no longer being used.
    
    footer: closes 2250
    JIRA-ID:GOVUKAPP-2250:
    
    * refactor  removes hello-world function
    
    scope: hello-world
    description: Removes the hello world function and updates the build job to use nx tsc
    plugin. Converts the tsconfig targets to ESM.
    
    footer: 2249
    JIRA-ID:GOVUKAPP-2249:
    
    * feat: waf for api gateway, part 1 (no acc tests) (#266)
    
    * refactor validation logic
    
    * fixing merge conflicts
    
    * refactor validation logic
    
    * fixing ci run
    
    * handling merge issues
    
    ---------
    
    Co-authored-by: Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
    Co-authored-by: Paz Saragi <76964496+pazsaragi@users.noreply.github.com>
    Co-authored-by: Brian Bayes <17807978+brianbayes@users.noreply.github.com>
    (cherry picked from commit cd98e27d194eab255d310e5eac6fe64358c728fa)

---
+++ auth/shared-signal-receiver/tests/unit/service/validation-service.unit.test.ts
@@ -0,0 +1,125 @@
+import { describe, it, expect, vi, beforeEach, Mock } from 'vitest';
+import { verifyUsername } from '../../../cognito/verify-users';
+import {
+  isUserValid,
+  isChangeTypeValid,
+} from '../../../service/validation-service';
+import { logMessages } from '../../../log-messages';
+
+vi.mock('../../../cognito/verify-users', () => ({
+  verifyUsername: vi.fn(),
+}));
+
+describe('validation-service', () => {
+  const schemaName =
+    'https://schemas.openid.net/secevent/caep/event-type/credential-change';
+  const jti = 'test-jti';
+  const username = 'urn:example:account:1234567890';
+
+  let consoleWarnMock = vi
+    .spyOn(console, 'warn')
+    .mockImplementation(() => undefined);
+  let consoleErrorMock = vi
+    .spyOn(console, 'error')
+    .mockImplementation(() => undefined);
+
+  beforeEach(() => {
+    consoleErrorMock.mockReset();
+    consoleWarnMock.mockReset();
+  });
+
+  describe('isUserValid', () => {
+    const verifyUsernameMock = verifyUsername as Mock;
+
+    beforeEach(() => {
+      verifyUsernameMock.mockReset();
+    });
+
+    it('returns true if verifyUsername resolves true', async () => {
+      verifyUsernameMock.mockResolvedValue(true);
+      const incomingRequest = {
+        jti,
+        events: {
+          [schemaName]: {
+            subject: { uri: username },
+          },
+        },
+      };
+      const result = await isUserValid(incomingRequest, schemaName);
+      expect(result).toBe(true);
+      expect(consoleWarnMock).not.toHaveBeenCalled();
+    });
+
+    it('returns false and logs warning if verifyUsername resolves false', async () => {
+      verifyUsernameMock.mockResolvedValue(false);
+      const incomingRequest = {
+        jti,
+        events: {
+          [schemaName]: {
+            subject: { uri: username },
+          },
+        },
+      };
+      const result = await isUserValid(incomingRequest, schemaName);
+      expect(result).toBe(false);
+      expect(consoleWarnMock).toHaveBeenCalledWith(
+        logMessages.SIGNAL_WARN_USER_NOT_FOUND,
+        { userId: username, correlationId: jti },
+      );
+    });
+  });
+
+  describe('isChangeTypeValid', () => {
+    it('returns true if changeType is undefined', () => {
+      const incomingRequest = {
+        jti,
+        events: {
+          [schemaName]: {
+            change_type: 'update',
+            subject: { uri: username },
+          },
+        },
+      };
+      const result = isChangeTypeValid(incomingRequest, schemaName);
+      expect(result).toBe(true);
+      expect(consoleErrorMock).not.toHaveBeenCalled();
+    });
+
+    it('returns true if incomingChangeType matches changeType', () => {
+      const incomingRequest = {
+        jti,
+        events: {
+          [schemaName]: {
+            change_type: 'update',
+            subject: { uri: username },
+          },
+        },
+      };
+      const result = isChangeTypeValid(incomingRequest, schemaName, 'update');
+      expect(result).toBe(true);
+      expect(consoleErrorMock).not.toHaveBeenCalled();
+    });
+
+    it('returns false and logs error if incomingChangeType does not match', () => {
+      const incomingRequest = {
+        jti,
+        events: {
+          [schemaName]: {
+            change_type: 'delete',
+            subject: { uri: username },
+          },
+        },
+      };
+      const result = isChangeTypeValid(incomingRequest, schemaName, 'update');
+      expect(result).toBe(false);
+      expect(consoleErrorMock).toHaveBeenCalledWith(
+        logMessages.SIGNAL_ERROR_UNKNOWN_CHANGE_TYPE,
+        {
+          userId: username,
+          correlationId: jti,
+          changeType: 'delete',
+        },
+      );
+    });
+  });
+});
