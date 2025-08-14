1f9791c5ba9bf436a3a756e77be7991b4d2d3b48
Surjeet Singh <surjeet.singh@digital.cabinet-office.gov.uk>
2025-08-14 11:11:12 +0100

    Feat: Shared Signal API gateway alarms (#278)
    * SS api gateway alarms
    
    (cherry picked from commit 54008c8b507a0fa598612cbdb59efbee36552b43)

---
+++ auth/tests/int/shared-signal-cloudwatch-alarms.test.ts
@@ -0,0 +1,74 @@
+import { describe, it, expect, afterAll } from 'vitest';
+import {
+  CloudWatchClient,
+  SetAlarmStateCommand,
+  DescribeAlarmsCommand,
+  DescribeAlarmHistoryCommand,
+} from '@aws-sdk/client-cloudwatch';
+import { testConfig } from '../common/config';
+const cloudWatchClient = new CloudWatchClient({ region: 'eu-west-2' });
+
+const alarmsToTest = [
+  testConfig.sharedSignalApiGatewayAlarm4xxErrors,
+  testConfig.sharedSignalApiGatewayAlarm5xxErrors,
+];
+
+describe.each(alarmsToTest)(
+  'Shared Signal API gateway cloudWatch Alarm: %s',
+  (alarmName) => {
+    it('should successfully execute its actions', async () => {
+      // Set the alarm state to ALARM
+      await cloudWatchClient.send(
+        new SetAlarmStateCommand({
+          AlarmName: alarmName,
+          StateValue: 'ALARM',
+          StateReason: 'Shared Signal API gateway ALARM state transition',
+        }),
+      );
+
+      // Verify the alarm state
+      const { MetricAlarms } = await cloudWatchClient.send(
+        new DescribeAlarmsCommand({ AlarmNames: [alarmName] }),
+      );
+
+      expect(MetricAlarms?.[0]?.StateValue).toBe('ALARM');
+
+      // Verify the alarm history executed an action
+      const { AlarmHistoryItems } = await cloudWatchClient.send(
+        new DescribeAlarmHistoryCommand({
+          AlarmName: alarmName,
+          HistoryItemType: 'Action',
+        }),
+      );
+      expect(AlarmHistoryItems?.[0]?.HistorySummary).toContain(
+        'Successfully executed action',
+      );
+
+      // Set the alarm state back to OK
+      await cloudWatchClient.send(
+        new SetAlarmStateCommand({
+          AlarmName: alarmName,
+          StateValue: 'OK',
+          StateReason: 'Shared Signal API gateway OK state transition',
+        }),
+      );
+      // Verify the alarm state
+      const { MetricAlarms: updatedMetricAlarms } = await cloudWatchClient.send(
+        new DescribeAlarmsCommand({ AlarmNames: [alarmName] }),
+      );
+      expect(updatedMetricAlarms?.[0]?.StateValue).toBe('OK');
+
+      // Verify the alarm history executed an action
+      const { AlarmHistoryItems: updatedAlarmHistoryItems } =
+        await cloudWatchClient.send(
+          new DescribeAlarmHistoryCommand({
+            AlarmName: alarmName,
+            HistoryItemType: 'Action',
+          }),
+        );
+      expect(updatedAlarmHistoryItems?.[0]?.HistorySummary).toContain(
+        'Successfully executed action',
+      );
+    });
+  },
+);
