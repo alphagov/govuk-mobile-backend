9d9d346d22c264d6c4b7e491ccbb0f420804c3d7
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2025-09-05 12:59:37 +0100

    feat: add powertools validation to healthcheck

--- services/auth/shared-signal-health-check/app.ts
+++ services/auth/shared-signal-health-check/app.ts
@@ -1,26 +1,30 @@
-import type { Context, ScheduledEvent } from 'aws-lambda';
 import { HealthCheckClient } from './client/health-check-client';
 import { initialiseHealthCheckService } from './init';
 import { logMessages } from './log-messages';
 import { logger } from './logger';
+import middy from '@middy/core';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import { errorMiddleware } from './middleware/global-error-handler';
+import type { ScheduledEvent } from 'aws-lambda';
 
 const healthCheckService = initialiseHealthCheckService();
 
-export const lambdaHandler = async (
-  event: ScheduledEvent,
-  context: Context,
-): Promise<void> => {
-  logger.addContext(context);
-  logger.logEventIfEnabled(event);
+export const lambdaHandler = middy<ScheduledEvent>()
+  .use(
+    injectLambdaContext(logger, {
+      correlationIdPath: 'id',
+    }),
+  )
+  .use(errorMiddleware())
+  .handler(async (event: ScheduledEvent): Promise<void> => {
+    logger.info(logMessages.HEALTH_CHECK_START, {
+      eventId: event.id,
+    });
 
-  logger.info(logMessages.HEALTH_CHECK_START, {
-    eventId: event.id,
+    const healthCheckClient = new HealthCheckClient(healthCheckService);
+    await healthCheckClient.performHealthCheck();
+
+    logger.info(logMessages.HEALTH_CHECK_END, {
+      eventId: event.id,
+    });
   });
-
-  const healthCheckClient = new HealthCheckClient(healthCheckService);
-  await healthCheckClient.performHealthCheck();
-
-  logger.info(logMessages.HEALTH_CHECK_END, {
-    eventId: event.id,
-  });
-};
