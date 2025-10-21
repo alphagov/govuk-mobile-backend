d5ba5e86d93f02f62a85d52b553c19bee6a14afd
Brian Bayes <brian.bayes@digital.cabinet-office.gov.uk>
2025-10-21 16:07:35 +0100

    chore: shared signal receiver common alarms

--- services/auth/tests/acc/shared-signal-lambda-alarms.test.ts
+++ services/auth/tests/acc/shared-signal-lambda-alarms.test.ts
@@ -69,6 +69,62 @@ const testCases: AlarmTestCase[] = [
       { Name: 'FunctionName', Value: { Ref: 'ChatAuthorizerFunction' } },
     ],
   },
+  {
+    name: 'SharedSignalAuthorizerConcurrency',
+    alarmName: `shared-signal-authorizer-concurrency`,
+    actionsEnabled: true,
+    namespace: 'AWS/Lambda',
+    metricName: 'ConcurrentExecutions',
+    alarmDescription:
+      'Alarm when the Authorizer Lambda concurrency approaches the limit, triggers at 80% of the limit.',
+    topicDisplayName: 'cloudwatch-alarm-topic',
+    statistic: 'Maximum',
+    period: 60,
+    evaluationPeriods: 1,
+    datapointsToAlarm: 1,
+    threshold: 800,
+    comparisonOperator: 'GreaterThanOrEqualToThreshold',
+    dimensions: [
+      { Name: 'FunctionName', Value: { Ref: 'ChatAuthorizerFunction' } },
+    ],
+  },
+  {
+    name: 'SharedSignalAuthorizerThrottles',
+    alarmName: `shared-signal-authorizer-throttles`,
+    actionsEnabled: true,
+    namespace: 'AWS/Lambda',
+    metricName: 'Throttles',
+    alarmDescription: 'Alarm when the Authorizer Lambda is being throttled.',
+    topicDisplayName: 'cloudwatch-alarm-topic',
+    period: 60,
+    statistic: 'Sum',
+    evaluationPeriods: 1,
+    datapointsToAlarm: 1,
+    threshold: 1,
+    comparisonOperator: 'GreaterThanOrEqualToThreshold',
+    dimensions: [
+      { Name: 'FunctionName', Value: { Ref: 'ChatAuthorizerFunction' } },
+    ],
+  },
+  {
+    name: 'SharedSignalAuthorizerTimeouts',
+    alarmName: `shared-signal-authorizer-timeout`,
+    actionsEnabled: true,
+    namespace: '${AWS::StackName}/Timeouts',
+    metricName: 'ChatAuthorizerFunctionTimeout',
+    alarmDescription:
+      'Alarm when the Authorizer Lambda function is timing out.',
+    topicDisplayName: 'cloudwatch-alarm-topic',
+    period: 60,
+    statistic: 'Sum',
+    evaluationPeriods: 1,
+    datapointsToAlarm: 1,
+    threshold: 5,
+    comparisonOperator: 'GreaterThanOrEqualToThreshold',
+    dimensions: [
+      { Name: 'FunctionName', Value: { Ref: 'ChatAuthorizerFunction' } },
+    ],
+  },
 ];
 
 describe.each(testCases)(
